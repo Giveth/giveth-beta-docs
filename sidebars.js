09bc4b116428038102fb9ee0f72a7b224e2037e1
Mohammad Ranbar Z <mranjbar.z2993@gmail.com>
2021-01-15 12:14:32 +0330

    Add simulation project docs

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   docsSidebar: {
-    'Getting Start': ['installation', 'contributors'],
+    'Getting Start': ['installation', 'contributors','giveth-1-simulation'],
     'Architecture': ['link-preview'],
     Reference: ['styleguide']
   }
